3df92a1f4683b21ebd7c239ae092d4abd7c2a656
02Naitsirk <iifrostii@outlook.com>
2021-02-26 19:52:50 -0500

    remove blank lines

--- src/com/github/naitsirk/Main.java
+++ src/com/github/naitsirk/Main.java
@@ -13,10 +13,8 @@ public class Main {
             int points = Integer.parseInt(args[0]);
             double probability = Double.parseDouble(args[1]);
             double returnRatio = Double.parseDouble(args[2]);
-
             int betA = calculateBet(points, probability, returnRatio);
             int betB = calculateBet(points, 1 - probability, returnRatio / (returnRatio - 1));
-
             System.out.println("Choice A: " + betA);
             System.out.println("Choice B: " + betB);
         } catch (Exception e) {
